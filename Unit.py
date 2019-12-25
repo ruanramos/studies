0b1fea54e8d753e14c64e7e928b6aeb068e77dae
ruanramos <ruan.ramos28@gmail.com>
2019-12-25 20:18:53 -0300

    Add Unit class

---
+++ Unit.py
@@ -0,0 +1,16 @@
+import RiotConsts
+
+class Unit:
+
+    def __init__(self, character_name, tier, items_id=[]):
+        self.character_id = "TFT2_" + character_name.title()
+        self.name = character_name.title()
+        self.items = items_id
+        self.rarity = -1
+        for cost, champs in RiotConsts.COST_CHAMPIONS.items():
+            if self.name in champs:
+                self.rarity = cost - 1
+        self.tier = tier
+
+    def level_up(self):
+        self.tier = self.tier + 1
