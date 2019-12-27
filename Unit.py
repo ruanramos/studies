d61f1d768d7fa49c65e5d3010d9432eb3f99e9ff
ruanramos <ruan.ramos28@gmail.com>
2019-12-27 20:57:54 -0300

    Add todo for qiyana case

--- Unit.py
+++ Unit.py
@@ -1,9 +1,24 @@
 import RiotConsts
 
+
 class Unit:
 
     def __init__(self, character_name, tier, active=False, items_id=[]):
         self.active = active
+        # TODO qiyana special case
+        # if character_name.lower() == "qiyanaw":
+        #     self.character_id = "TFT2_QiyanaWind"
+        #     self.name = "QiyanaWind"
+        # elif character_name.lower() == "qiyanai":
+        #     self.character_id = "TFT2_QiyanaInferno"
+        #     self.name = "QiyanaInferno"
+        # elif character_name.lower() == "qiyanao":
+        #     self.character_id = "TFT2_QiyanaOcean"
+        #     self.name = "QiyanaOcean"
+        # elif character_name.lower() == "qiyanam":
+        #     self.character_id = "TFT2_QiyanaMountain"
+        #     self.name = "QiyanaMountain"
+        # else:
         self.character_id = "TFT2_" + character_name.title()
         self.name = character_name.title()
         self.items = items_id
