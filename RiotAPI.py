a24d33bbf3273bbd7cd26103faa2bab0b0f4541e
ruanramos <ruan.ramos28@gmail.com>
2019-12-27 21:04:50 -0300

    Rename methods to PEP8, remove twisted treeline

--- RiotAPI.py
+++ RiotAPI.py
@@ -19,7 +19,7 @@ class RiotAPI(object):
         )
         return response.json()
 
-    def _requestTFTMatches(self, api_url, count=1, params={}):
+    def _request_tft_matches(self, api_url, count=1, params={}):
         args = {"api_key": self.api_key, "count": count}
         for key, value in params.items():
             if key not in args:
@@ -47,28 +47,29 @@ class RiotAPI(object):
         api_url = Consts.URL["tft_matches_by_player_puuid"].format(
             Consts.API_VERSIONS["tft-match"], puuid
         )
-        return self._requestTFTMatches(api_url, count)
+        return self._request_tft_matches(api_url, count)
 
     def get_tft_match_by_match_id(self, match_id):
         api_url = Consts.URL["tft_match_by_match_id"].format(
             Consts.API_VERSIONS["tft-match"], match_id
         )
-        return self._requestTFTMatches(api_url)
+        return self._request_tft_matches(api_url)
 
+    # noinspection SpellCheckingInspection
     def get_masteries_by_summoner_id(self, summoner_id):
         api_url = Consts.URL["all_masteries_by_summoner_id"].format(
             Consts.API_VERSIONS["champion-mastery"], summoner_id
         )
         return self._request(api_url)
 
-    def challengerleagues_Solo_5x5(self, queue):
+    def challenger_leagues_solo_5x5(self, queue):
         api_url = Consts.URL["challengerleagues"].format(Consts.QUEUES[0])
         return self._request(api_url)
 
-    def challengerleagues_Flex_SR(self, queue):
+    def challenger_leagues_flex_sr(self, queue):
         api_url = Consts.URL["challengerleagues"].format(Consts.QUEUES[1])
         return self._request(api_url)
 
-    def challengerleagues_Flex_TT(self, queue):
-        api_url = Consts.URL["challengerleagues"].format(Consts.QUEUES[2])
-        return self._request(api_url)
+    # def challengerleagues_Flex_TT(self, queue):
+    #     api_url = Consts.URL["challengerleagues"].format(Consts.QUEUES[2])
+    #     return self._request(api_url)
